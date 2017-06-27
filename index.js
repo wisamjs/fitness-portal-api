22332c0a0902ef31c624854b40e4166ed0b8e35c
wisamjs <wisamzaghal@gmail.com>
2017-06-26 22:34:31 -0400

    test cors with koa-router

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 const koa = require('koa');
 const route = require('koa-route');
+const router = require('koa-router')();
 var bodyParser = require('koa-bodyparser');
 const app = new koa();
 
 const routes = require('./src/routes/routes');
 
+const CORS = async (ctx, next) => {
+	ctx.set('Access-Control-Allow-Origin', '*');
+	await next();
+}
+
 app.use(bodyParser());
-app.use(route.get('/exercises', routes.exercises.list));
-app.use(route.get('/exercises/name/:name', routes.exercises.findByName));
-app.use(route.get('/exercises/id/:id', routes.exercises.findByName));
-app.use(route.post('/exercise/', routes.exercises.add));
 
-app.use(route.get('/dates', routes.dates.list));
-app.use(route.post('/dates', routes.dates.add));
+router.use('*', CORS);
+router.get('/workouts', routes.workouts.list);
+app.use(router.routes());
+app.use(router.allowedMethods());
 
-app.use(route.get('/workingSets', routes.workingSets.list));
-app.use(route.post('/workingSets', routes.workingSets.add));
+// app.use(route.get('/exercises', routes.exercises.list));
+// app.use(route.get('/exercises/name/:name', routes.exercises.findByName));
+// app.use(route.get('/exercises/id/:id', routes.exercises.findByName));
+// app.use(route.post('/exercise/', routes.exercises.add));
 
-app.use(route.get('/workouts', routes.workouts.list));
-app.use(route.post('/workouts', routes.workouts.add));
+// app.use(route.get('/dates', routes.dates.list));
+// app.use(route.post('/dates', routes.dates.add));
+
+// app.use(route.get('/workingSets', routes.workingSets.list));
+// app.use(route.post('/workingSets', routes.workingSets.add));
+
+// app.use(route.get('/workouts', routes.workouts.list));
+// app.use(route.post('/workouts', routes.workouts.add));
 
 app.listen(process.env.PORT || 8080);
